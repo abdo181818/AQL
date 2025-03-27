d3d4c14ca8ce5781af7c9cfefd35eff0bd8d0d04
lebssir abdelwahab <abdelwahab@gmail.com>
2025-03-27 09:15:56 +0100

    TP0EXO0

---
+++ src/test/java/org/example/RomanNumeralTestt.java
@@ -0,0 +1,17 @@
+package org.example;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RomanNumeralTestt {
+
+    @Test
+    public void singleNumber() {
+        RomanNumeral roman = new RomanNumeral();
+        int result = roman.convert("I");
+
+        Assertions.assertEquals(1, result);
+    }
+
+}
